6a96a8bdb25d02da71fb3c633004854c15a01ea5
Supakarn Laorattanakul <wearedprompt@gmail.com>
2023-04-18 00:46:31 +0700

    inject config

--- src/graphql/WGGraphQL.module.ts
+++ src/graphql/WGGraphQL.module.ts
@@ -2,13 +2,16 @@ import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo'
 import { Module } from '@nestjs/common'
 import { GraphQLModule } from '@nestjs/graphql'
 import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default'
+import { ConfigModule, ConfigService } from '@nestjs/config'
 import * as Modules from '~/modules'
 
 @Module({
   imports: [
     GraphQLModule.forRootAsync<ApolloDriverConfig>({
+      imports: [ConfigModule],
+      inject: [ConfigService],
       driver: ApolloDriver,
-      useFactory: async () => ({
+      useFactory: async (_configService: ConfigService) => ({
         autoSchemaFile: 'schema.gql',
         playground: false,
         plugins: [ApolloServerPluginLandingPageLocalDefault()],
